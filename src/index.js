497a500ed4e18ff0952195b36c6a5b7004df6b69
AndMkh7 <andmkhart77@gmail.com>
2021-06-05 21:36:29 +0400

    Adding CSS Styles, Using Bootstrap + making code more compact

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import AppHeader from"./components/app-header";
-import SearchPanel from"./components/search-panel";
-import TodoList from"./components/todo-list";
-import ItemStatusFilter from "./components/item-status-filter";
-
-import"./components/index.css";
-   
-const App =()=>{
-
-    const todoData = [
-        {label : "Driknk Coffee", important : false , id: 1},
-        {label : "Build React App", important : true, id: 2 }, 
-        {label : "Have a lunch", important : false, id: 3}
-    ];
-
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList  todos = {todoData}/>
-            <ItemStatusFilter />
-        </div>
-       );
-} ;
+import App from './components/app';
 
 
-
-ReactDOM.render(<App/>, document.getElementById("root") ) ;
+ReactDOM.render(<App />,
+    document.getElementById('root'));
